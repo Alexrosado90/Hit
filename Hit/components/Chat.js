76cfda7be4d70f7d064d1ba2533cdee06aad1002
Alex Rosado <Ace@ool-ad02054b.dyn.optonline.net>
2018-08-09 15:41:15 -0400

    chat page functioning, not able to store messages yet

--- Hit/components/Chat.js
+++ Hit/components/Chat.js
@@ -1,24 +1,97 @@
 import React, {Component } from 'react';
 import { View, Text } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import firebase from 'firebase';
+import { StackNavigator } from 'react-navigation';
 
-
+let name, uid, email;
 
 export default class Chat extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            messages: []
+        };
+
+        let name, uid, email;
+
+        this.user = firebase.auth().currentUser;
+       // console.log("user" + this.user.uid)
+        //this.friend = this.props.friend
+
+        const { params } = this.props.navigation.state;
+        //uid = params.uid;
+        //name = params.name;
+        //email = params.email
+        console.log("user:" + uid);
+
+        this.chatRef = this.getRef().child('chat/' + this.generateChatId())
+        this.chatRefData = this.chatRef.orderByChild('order')
+        this.onSend = this.onSend.bind(this);
     }
 
-    state = {
-        messages: [],
-    };
+    generateChatId() {
+        if(this.user.uid > uid)
+        return `${this.user.uid}-${uid}`;
+        else return `${uid}-${this.user.uid}`;
+    }
+
+    getRef() {
+        return firebase.database().ref();
+    }
+
+    listenForItems(chatRef) {
+        chatRef.on("value", snap => {
+            let items = [];
+            snap.forEach(child => {
+                items.push({
+                    _id: child.val().createdAt,
+                    text: child.val().text,
+                    createdAt: new Date(child.val().createdAt),
+                    user: {
+                        _id: child.val().uid
+                    }
+
+                })
+            })
+
+            this.setState({
+                loading: false,
+                messages: items
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.listenForItems(this.chatRefData);
+    }
+
+    componentWillUnmount() {
+        this.chatRefData.off();
+    }
+
+    onSend(messages = []) {
+        messages.forEach(message => {
+            let now = new Date().getTime();
+            this.chatRef.push({
+                id: now,
+                text: message.text,
+                createdAt: now,
+                uid: this.user.uid,
+                order: -1 * now
+            });
+        });
+    }
 
 
     render() {
         return (
             <GiftedChat
             messages={this.state.messages}
+            onSend={this.onSend.bind(this)}
+            user={{ _id: this.user.uid}}
             />
         )
     }
 }
+
